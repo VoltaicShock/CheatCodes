24964432eaa59adbcbcc1e2be880c0a4b1ae372c
VoltaicShock <tsaundersjr@gmail.com>
2018-06-24 19:47:57 -0400

    Framework to get the cheat codes inputfield and button into the mod.

---
+++ CheatCodes/CheatCodes/UI/CheatCodesOnDemandLoader.cs
@@ -0,0 +1,36 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace CheatCodes.UI
+{
+    public class CheatCodesOnDemandLoader : ILoadingExtension
+    {
+        private static CheatCodesUserInterfaceComponent component = null;
+
+        public void OnCreated(ILoading loading)
+        {
+        }
+
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode == LoadMode.LoadGame || mode == LoadMode.NewGame)
+            {
+                GameObject obj = new GameObject("CheatCodesUserInterfaceComponent");
+                component = obj.AddComponent<CheatCodesUserInterfaceComponent>();
+            }
+        }
+
+        public void OnLevelUnloading()
+        {
+            if (component != null)
+            {
+                Object.Destroy(component.gameObject);
+                component = null;
+            }
+        }
+
+        public void OnReleased()
+        {
+        }
+    }
+}
---
+++ CheatCodes/CheatCodes/UI/CheatCodesUserInterfaceComponent.cs
@@ -0,0 +1,34 @@
+﻿using ColossalFramework.Plugins;
+using UnityEngine;
+
+namespace CheatCodes.UI
+{
+    public class CheatCodesUserInterfaceComponent : MonoBehaviour
+    {
+        private bool CtrlCmdDown
+        {
+            get
+            {
+                return Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) ||
+                       Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand);
+            }
+        }
+
+        private bool ShiftDown
+        {
+            get
+            {
+                return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.C) && CtrlCmdDown && ShiftDown)
+            {
+                //Todo: Load UI Panel with input field and button
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Cheat Codes Activated");
+            }
+        }
+    }
+}
